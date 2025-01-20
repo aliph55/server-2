f5083af624c8d097140fc8f313ae8ee67fefd129
Ali Pourhassan <67645398+aliph55@users.noreply.github.com>
2025-01-20 21:53:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from "express";
-import Stripe from "stripe";
-import { STRIPE_SECRET_KEY } from "./constants";
-
-const stripeSecretKey = STRIPE_SECRET_KEY;
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const stripe = require("stripe")(
+  "pk_live_51NOMvzJzZzpXlpJ7GwHfXu7leBnDN8B4fQ1S0loPxQgCxUdrPxpzOzQt8aQbeEsrhol1VAVeEHz3XKOXXY3sGz4b0056T5j4h8"
+); // Stripe secret anahtarınızı buraya koyun
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
+
 // Google Pay ödeme işleme rotası
 app.post("/create-payment-intent", async (req, res) => {
   try {
     // Ödeme miktarını ve para birimini istemciden alın
-    const { amount, currency, email } = req.body;
+    const { amount, currency } = req.body;
 
     // PaymentIntent oluşturun
     const paymentIntent = await stripe.paymentIntents.create({
